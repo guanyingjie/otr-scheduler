c8a922147f8b166fe6691ade4229d81a0a048ce0
Yingjie <yingjie.guan@thoughtworks.com>
2021-11-05 11:19:55 +0800

    Initial commit

---
+++ src/main/java/com/yingjie/otrscheduler/OtrSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.yingjie.otrscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtrSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtrSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yingjie/otrscheduler/OtrSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yingjie.otrscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtrSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
